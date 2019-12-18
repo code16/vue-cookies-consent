23b586593a014bba12e53d749348528cc02c2b52
antoine <antoine.guingand@wanadoo.fr>
2019-12-18 17:29:52 +0100

    Bump dependencies & set axios as external dependency

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,9 @@ let mix = require('laravel-mix');
 mix.js('src/index.js', 'dist/vue-cookies-consent.js')
     .webpackConfig({
         output: {
-            libraryTarget:'umd'
-        }
+            libraryTarget: 'umd'
+        },
+        externals: [
+            'axios',
+        ],
     });
